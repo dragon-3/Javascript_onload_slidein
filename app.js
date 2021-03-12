76e04a4aaf65847d0f5852860abbc5d866d39876
dragon_3 <seannarron9@gmail.com>
2021-03-12 10:14:07 +0900

    First commit

---
+++ app.js
@@ -0,0 +1,15 @@
+//onload slidein
+var myIndex = 0;
+    carousel();
+    
+    function carousel() {
+        var i;
+        var x = document.getElementsByClassName("login contact style");
+        for (i = 0; i < x.length; i++) {
+            x[i].style.display = "none";  
+        }
+        myIndex++;
+        if (myIndex > x.length) {myIndex = 1}    
+        x[myIndex-1].style.display = "block";  
+        setTimeout(carousel, 2500);    
+    }
